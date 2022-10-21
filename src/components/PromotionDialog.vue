90c36deb8b2c280f8cdc562a23ee4399deccfacd
Luca Hendrik Helms <lucahendrik.dev@gmail.com>
2022-10-21 22:49:41 +0200

    improve dialog

--- src/components/PromotionDialog.vue
+++ src/components/PromotionDialog.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { onMounted, ref, toRef } from 'vue';
+import { ref } from 'vue';
 import type { PieceColor } from 'chess.js';
 import type { Promotion } from '@/typings/Chessboard';
 
-const props = defineProps({
+defineProps({
   turnColor: {
     type: String as () => PieceColor,
     required: true,
@@ -15,16 +15,6 @@ const emit = defineEmits<{
 }>();
 
 const dialogEl = ref<HTMLDialogElement | null>(null);
-const currentTurnColor = toRef(props, 'turnColor');
-const promotionColor = ref<PieceColor>();
-
-onMounted(() => {
-  if (currentTurnColor.value === 'w') {
-    promotionColor.value = 'b';
-  } else {
-    promotionColor.value = 'w';
-  }
-});
 
 function promotionSelected(e: Event) {
   const clickedEl = e.target as HTMLDivElement;
@@ -37,49 +27,55 @@ function promotionSelected(e: Event) {
 </script>
 
 <template>
-  <Teleport v-if="promotionColor" to="cg-board">
+  <Teleport to="cg-board">
     <dialog
       ref="dialogEl"
       open
       @click="promotionSelected"
       @touchstart.passive="promotionSelected"
     >
-      <div v-if="promotionColor === 'b'" class="promotion-container">
-        <div class="promotion queen-w-promotion">
+      <div v-if="turnColor === 'w'" class="promotion-container">
+        <div class="queen-w-promotion">
           <button data-piece="q">
             <span class="sr-only">Queen</span>
           </button>
         </div>
-        <div class="promotion knight-w-promotion">
+        <div class="knight-w-promotion">
           <button data-piece="n">
             <span class="sr-only">Knight</span>
           </button>
         </div>
-        <div class="promotion rook-w-promotion">
+        <div class="rook-w-promotion">
           <button data-piece="r">
             <span class="sr-only">Rook</span>
           </button>
         </div>
-        <div class="promotion bishop-w-promotion">
+        <div class="bishop-w-promotion">
           <button data-piece="b">
             <span class="sr-only">Bishop</span>
           </button>
         </div>
       </div>
-      <div v-if="promotionColor === 'w'" class="promotion-container">
-        <div class="promotion queen-b-promotion">
+      <div v-if="turnColor === 'b'" class="promotion-container">
+        <div class="queen-b-promotion">
           <button data-piece="q">
             <span class="sr-only">Queen</span>
           </button>
         </div>
-        <div class="promotion knight-b-promotion">
-          <button data-piece="n"><span class="sr-only">Knight</span></button>
+        <div class="knight-b-promotion">
+          <button data-piece="n">
+            <span class="sr-only">Knight</span>
+          </button>
         </div>
-        <div class="promotion rook-b-promotion">
-          <button data-piece="r"><span class="sr-only">Rook</span></button>
+        <div class="rook-b-promotion">
+          <button data-piece="r">
+            <span class="sr-only">Rook</span>
+          </button>
         </div>
-        <div class="promotion bishop-b-promotion">
-          <button data-piece="b"><span class="sr-only">Bishop</span></button>
+        <div class="bishop-b-promotion">
+          <button data-piece="b">
+            <span class="sr-only">Bishop</span>
+          </button>
         </div>
       </div>
     </dialog>
